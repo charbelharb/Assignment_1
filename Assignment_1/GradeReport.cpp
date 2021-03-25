6648486bd5f93dd00a0a87852e5239dffa6dd304
Charbel Harb <s@scorz.org>
2021-03-26 00:48:49 +0200

    wip

---
+++ Assignment_1/GradeReport.cpp
@@ -0,0 +1,1 @@
+#include "GradeReport.h"
---
+++ Assignment_1/GradeReport.h
@@ -0,0 +1,13 @@
+#ifndef GardeReport.h
+#define GardeReport.h
+
+class GradeReport
+{
+private:
+	unsigned int _id;
+	char* _firstName;
+	char* _lastName;
+	unsigned short _numberOfCredits;
+	unsigned short _age;
+};
+#endif
